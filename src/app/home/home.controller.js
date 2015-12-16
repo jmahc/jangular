8b3036f5c7af0cdfd387e2fe220f833672e0685a
Jordan McArdle <Jordan.McArdle@2-com.net>
2015-12-16 16:03:59 -0600

    Navbar, other two pages, scrollmagic, and video background all need to be fixed

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -6,26 +6,14 @@
     .controller('HomeController', HomeController);
 
   /** @ngInject */
-  function HomeController($timeout, Portfolio, toastr) {
+  function HomeController(Portfolio) {
     var vm = this;
 
     vm.portfolioItems = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1450215881049;
-    vm.showToastr = showToastr;
-
     activate();
 
     function activate() {
       getPortfolio();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
     }
 
     function getPortfolio() {
