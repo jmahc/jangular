a00f4e53120fee4ddc3d8c8bfb455a8ece40b1bb
Jordan McArdle <Jordan.McArdle@2-com.net>
2015-12-18 14:37:32 -0600

    test for node build4

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
+var gzippo = require('gzippo'),
+    express = require('express'),
+    app = express();
 
-  app.use(express.logger('dev'));
   app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 5000);
+  app.set('port', (process.env.PORT || 5000));
+  app.use(express.static(__dirname + '/public'));
+  // views is directory for all template files
+  app.set('views', __dirname + '/views');
+  //app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
+
+
+  app.listen(app.get('port'), function() {
+    console.log('The party is happening on port ', app.get('port'));
+  }).listen(5000);
